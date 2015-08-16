2c31e7334c007e29290b3088aaf2a2fe2eb67977
Viacheslav Ivanov <slava.eth@gmail.com>
2015-08-15 21:25:42 -0700

    added collision detection

--- js/states/game.state.js
+++ js/states/game.state.js
@@ -1,22 +1,30 @@
 ;(function (Phaser, app) {
   app.GameState = {
     create: function () {
+      // create ground
       this.ground = this.game.add.sprite(0, 500, 'ground')
       this.game.physics.arcade.enable(this.ground)
       this.ground.body.allowGravity = false
       this.ground.body.immovable = true
 
+      // create platform
       this.platform = this.game.add.sprite(0, 300, 'platform')
       this.game.physics.arcade.enable(this.platform)
       this.platform.body.allowGravity = false
       this.platform.body.immovable = true
 
+      // create player
       this.player = this.game.add.sprite(100, 200, 'player', 3)
       this.player.anchor.setTo(0.5)
       this.player.animations.add('walking', [0, 1, 2, 1], 6, true)
       this.game.physics.arcade.enable(this.player)
     },
 
-    update: function () {}
+    update: function () {
+      this.game.physics.arcade.collide(this.player, this.ground, this.landed)
+      this.game.physics.arcade.collide(this.player, this.platform, this.landed)
+    },
+
+    landed: function (player, ground) {}
   }
 })(window.Phaser, window.app)
